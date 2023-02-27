5d2baf68daf1c18908caf6934a25135ba29c5cdf
efezino idisi <enduranceidisi13@gmail.com>
2023-02-27 22:20:12 +0100

    update comment

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
- * @parent: pointer to the node to insert the left child
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right child
  * @value: value to store in the new node
  *
  * Return: pointer to new node or NULL
