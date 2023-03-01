04c608dde5965d3fb690593ca18499a56a9fa6c7
efezino idisi <enduranceidisi13@gmail.com>
2023-03-01 18:24:10 +0100

    task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,43 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node
+ *
+ * Return: 1 if true otherwise 0
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = tree_height(tree->left);
+	right = tree_height(tree->right);
+
+	if (left != right)
+		return (0);
+	return (1);
+}
+
+/**
+ * tree_height - calculate height of a binary tree
+ * @tree: pointer to root node of tree
+ *
+ * Return: height of tree or 0 if NULL
+ */
+
+size_t tree_height(const binary_tree_t *tree)
+{
+	size_t subtree_l, subtree_r;
+
+	if (tree == NULL)
+		return (0);
+
+	subtree_l = tree_height(tree->left);
+	subtree_r = tree_height(tree->right);
+
+	return ((subtree_l > subtree_r ? subtree_l : subtree_r) + 1);
+}
