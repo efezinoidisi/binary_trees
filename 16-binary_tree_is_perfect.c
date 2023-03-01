8134fd0b63958900a46c0503f6a83d4a48d5a770
efezino idisi <enduranceidisi13@gmail.com>
2023-03-01 19:13:31 +0100

    possible fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -19,7 +19,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (left != right)
 		return (0);
-	return (binary_tree_is_full(tree));
+	if (binary_tree_is_full(tree))
+		return (1);
+	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 }
 
 /**
