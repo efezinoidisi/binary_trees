509ef4095586617a0be6e14bebce5fbb84acc111
efezino idisi <enduranceidisi13@gmail.com>
2023-03-01 18:54:12 +0100

    update task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -19,7 +19,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (left != right)
 		return (0);
-	return (1);
+	return (binary_tree_is_full(tree));
 }
 
 /**
@@ -40,4 +40,30 @@ size_t tree_height(const binary_tree_t *tree)
 	subtree_r = tree_height(tree->right);
 
 	return ((subtree_l > subtree_r ? subtree_l : subtree_r) + 1);
+}
+
+/**
+ * binary_tree_is_full - checks if a binary tree is a full binary tree
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if True or 0 if false
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	if (!tree->left || !tree->right)
+		return (0);
+
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+
+	return (left && right);
 }
