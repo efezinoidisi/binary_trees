582406000e4c7cc56970ebc3ce20a1f2af4b6447
efezino idisi <enduranceidisi13@gmail.com>
2023-03-02 10:32:15 +0100

    revert

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stdio.h>
 
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
@@ -10,20 +9,17 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int height, nodes, expected;
+	int left, right;
 
 	if (tree == NULL)
 		return (0);
 
-	height = tree_height(tree);
-	expected = get_power(height, 2) - 1;
-	nodes = binary_tree_size(tree);
-	printf("nodes: %d height: %d expected: %d\n", nodes, height, expected);
+	left = tree_height(tree->left);
+	right = tree_height(tree->right);
 
-	if (expected != nodes)
+	if (left != right)
 		return (0);
-
-	return (1);
+	return ((binary_tree_is_full(tree)));
 }
 
 /**
@@ -47,42 +43,27 @@ size_t tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_size - calculates the size of a binary tree
- * @tree: pointer to the root node of the tree
+ * binary_tree_is_full - checks if a binary tree is a full binary tree
+ * @tree: pointer to the root node of the tree to check
  *
- * Return: size of tree or 0 if tree is NULL
+ * Return: 1 if True or 0 if false
  */
 
-size_t binary_tree_size(const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
-	size_t size_l, size_r;
+	int left, right;
 
 	if (tree == NULL)
 		return (0);
-	size_l = tree->left ? binary_tree_size(tree->left) : 0;
-	size_r = tree->right ? binary_tree_size(tree->right) : 0;
 
-	return (size_l + size_r + 1);
-}
+	if (!tree->left && !tree->right)
+		return (1);
 
+	if (!tree->left || !tree->right)
+		return (0);
 
-/**
- * get_power - calculate the power of a number
- * @n: number to get power
- * @base: base number
- *
- * Return: power of number
- */
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
 
-size_t get_power(size_t n, size_t base)
-{
-	size_t result = 1;
-
-	while (n != 0)
-	{
-		result *= base;
-		n--;
-	}
-
-	return (result);
+	return (left && right);
 }
