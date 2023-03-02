060b0f3f932198a4aecfcc2de3c0a4b867b74402
efezino idisi <enduranceidisi13@gmail.com>
2023-03-03 00:29:22 +0100

    possible fix

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -12,6 +12,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
 	size_t depth_first, depth_second;
+	binary_tree_t *parent1, *parent2;
 
 	if (first == NULL || second == NULL)
 		return (NULL);
@@ -24,10 +25,28 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	depth_first = binary_tree_depth(first);
 	depth_second = binary_tree_depth(second);
 
-	if (depth_first == depth_second && first->parent != second->parent)
-		return (first->parent->parent);
+	if (depth_first == depth_second)
+		return (first->parent);
 
-	return (depth_first < depth_second ? first->parent : second->parent);
+	if (depth_first > depth_second)
+	{
+		parent1 = first->parent;
+		parent2 = second->parent;
+	}
+	else
+	{
+		parent1 = second->parent;
+		parent2 = first->parent;
+	}
+
+	while (parent2 != parent1)
+	{
+		if (parent1->parent == NULL)
+			break;
+		parent1 = parent1->parent;
+	}
+
+	return (parent1);
 }
 
 /**
